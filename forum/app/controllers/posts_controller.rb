bbbecc8b3e0a959e41fb900e2511676dd5f1ea14
massud <massudgnan@gmail.com>
2015-05-24 19:43:48 +0100

    post and user associated

--- forum/app/controllers/posts_controller.rb
+++ forum/app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def show
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to @post
     else
